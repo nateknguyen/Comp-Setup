fa8705ad55f128e733c09883036ff49a1b66d9e6
Nathan Nguyen <na7thana@gmail.com>
2022-09-22 18:54:10 -0700

    Update brew-install-script.sh

--- install_scripts/mac_install_scripts_brew/brew-install-script.sh
+++ install_scripts/mac_install_scripts_brew/brew-install-script.sh
@@ -29,7 +29,6 @@ brew install --cask steam
 brew install --cask alt-tab
 brew install --cask discord
 brew install --cask slack
-brew install --cask zoom
 brew install --cask mullvadvpn
 brew install --cask libreoffice
 brew install --cask calibre
@@ -42,6 +41,8 @@ brew install git
 brew install python
 brew install node
 brew install yt-dlp
+
+brew install --cask zoom
 
 brew analytics off
 
